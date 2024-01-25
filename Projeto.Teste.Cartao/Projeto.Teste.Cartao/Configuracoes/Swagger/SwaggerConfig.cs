10a04e5ec8b09c5e90f471e811e3f4b88397a604
Charles Wilton Jardim Gentilin <c.gentilin@senff.com.br>
2024-01-25 20:18:34 -0300

    melhoria configs swagger

---
+++ Projeto.Teste.Cartao/Projeto.Teste.Cartao/Configuracoes/Swagger/SwaggerConfig.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+
+namespace Projeto.Teste.Cartao.Configuracoes.Swagger
+{
+    public static class SwaggerConfig
+    {
+        public static void AddSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo()
+                {
+                    Version = "v1",
+                    Title = "Microserviço Contexto Cartao",
+                    Description = "API projeto teste Charles Jardim Gentilin"
+                });
+
+                c.CustomSchemaIds(x => x.FullName);
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+
+            });
+        }
+
+    }
+}
